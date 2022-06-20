262e99bd90e0abcf862a82fe3e17e34a0b85f7ea
조재철 <rhsnqk@gmail.com>
2022-06-21 02:04:26 +0900

    start spring boot board project1

---
+++ boardStep1/src/main/java/com/joisfe/boardStep1/BoardStep1Application.java
@@ -0,0 +1,13 @@
+package com.joisfe.boardStep1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardStep1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardStep1Application.class, args);
+	}
+
+}
---
+++ boardStep1/src/test/java/com/joisfe/boardStep1/BoardStep1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joisfe.boardStep1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardStep1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
